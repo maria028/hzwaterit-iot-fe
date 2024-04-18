6036ec4f854a3bfb2890cd664be7710a5a6f929d
pzy <1012839072@qq.com>
2024-04-18 17:16:39 +0800

    fix: 员工列表取消左侧部门树

--- src/views/System/Employee/index.vue
+++ src/views/System/Employee/index.vue
@@ -1,95 +1,84 @@
 <template>
-    <el-row :gutter="16" style="height: 100%">
-        <el-col :span="4" style="height: 100%">
-            <el-card>
-                <CTree :data="treeData" defaultKey="0" @nodeClick="handlerNodeClick" />
-            </el-card>
-        </el-col>
-        <el-col :span="20" style="height: 100%">
-            <CSearchTable
-                tableName="员工列表"
-                :data="tableData"
-                @search="getTableData"
-                @clear="reset"
-                :loading="loading"
-                :total="rows"
-                v-model:currentPage="queryModel.pageNum"
-                v-model:pageSize="queryModel.pageSize"
-            >
-                <template #search>
-                    <CSearchBarItem label="姓名">
-                        <el-input v-model="queryModel.name" clearable maxlength="30" placeholder="请输入姓名" />
-                    </CSearchBarItem>
-                    <CSearchBarItem label="手机号码">
-                        <el-input v-model="queryModel.phoneNumber" clearable maxlength="11" placeholder="请输入手机号码" />
-                    </CSearchBarItem>
-                    <CSearchBarItem label="状态">
-                        <el-select v-model="queryModel.statusCode" placeholder="请选择" clearable>
-                            <el-option
-                                v-for="employeeStatusDict in employeeStatusDicts"
-                                :key="employeeStatusDict.code"
-                                :label="employeeStatusDict.name"
-                                :value="employeeStatusDict.code"
-                            />
-                        </el-select>
-                    </CSearchBarItem>
+    <CSearchTable
+        tableName="员工列表"
+        :data="tableData"
+        @search="getTableData"
+        @clear="reset"
+        :loading="loading"
+        :total="rows"
+        v-model:currentPage="queryModel.pageNum"
+        v-model:pageSize="queryModel.pageSize"
+    >
+        <template #search>
+            <CSearchBarItem label="部门">
+                <el-tree-select v-model="queryModel.departmentId" :data="treeData" :props="{ children: 'children', label: 'name' }" node-key="id" clearable check-strictly />
+            </CSearchBarItem>
+            <CSearchBarItem label="姓名">
+                <el-input v-model="queryModel.name" clearable maxlength="30" placeholder="请输入姓名" />
+            </CSearchBarItem>
+            <CSearchBarItem label="手机号码">
+                <el-input v-model="queryModel.phoneNumber" clearable maxlength="11" placeholder="请输入手机号码" />
+            </CSearchBarItem>
+            <CSearchBarItem label="状态">
+                <el-select v-model="queryModel.statusCode" placeholder="请选择" clearable>
+                    <el-option v-for="employeeStatusDict in employeeStatusDicts" :key="employeeStatusDict.code" :label="employeeStatusDict.name" :value="employeeStatusDict.code" />
+                </el-select>
+            </CSearchBarItem>
+        </template>
+        <template #tableLeft> </template>
+        <template #tableRight>
+            <el-button v-permission="'POST/employee'" type="primary" @click="handleAdd">新增</el-button>
+        </template>
+        <template #columns>
+            <el-table-column type="index" label="序号" width="80" />
+            <el-table-column label="头像" width="80">
+                <template #default="scope">
+                    <el-avatar :size="30" v-if="scope.row.avatarImgUrl" :src="scope.row.avatarImgUrl" />
+                    <el-avatar :size="30" v-else>{{ scope.row.name.substr(0, 1) }}</el-avatar>
                 </template>
-                <template #tableLeft> </template>
-                <template #tableRight>
-                    <el-button v-permission="'POST/employee'" type="primary" @click="handleAdd">新增</el-button>
+            </el-table-column>
+            <el-table-column label="姓名" prop="name" width="100" />
+            <el-table-column label="手机号码" prop="phoneNumber" width="120" />
+            <el-table-column label="帐号状态" prop="accountStatus" width="100" />
+            <el-table-column label="状态" prop="status" width="100" />
+            <el-table-column label="部门" prop="departmentNames" width="100" />
+            <el-table-column label="职位" prop="positionNames" width="100" />
+            <el-table-column label="角色" prop="roleName" width="100" />
+            <el-table-column label="性别" prop="gender" width="80" />
+            <el-table-column label="工号" prop="jobNumber" width="100" />
+            <el-table-column label="邮箱" prop="email" width="200" />
+            <el-table-column label="创建时间" prop="createGmt" width="200" />
+            <el-table-column label="修改时间" prop="modifiedGmt" width="200" />
+            <el-table-column label="操作" fixed="right" width="220">
+                <template #default="scope">
+                    <el-button type="primary" text @click="handleEdit(scope.row.id)" v-permission="'PUT/employee'">修改</el-button>
+                    <el-button
+                        type="primary"
+                        text
+                        @click="handlerUpdateAccountStatus(scope.row.id, 1)"
+                        v-if="scope.row.accountStatusCode == '2'"
+                        v-permission="'PUT/employee-account-status'"
+                        >启用</el-button
+                    >
+                    <el-button
+                        type="primary"
+                        text
+                        @click="handlerUpdateAccountStatus(scope.row.id, 2)"
+                        v-if="scope.row.accountStatusCode == '1'"
+                        v-permission="'PUT/employee-account-status'"
+                        >禁用</el-button
+                    >
+                    <el-button type="primary" text @click="handlerUpdateStatus(scope.row.id, 1)" v-if="scope.row.statusCode == '2'" v-permission="'PUT/employee-status'"
+                        >在职</el-button
+                    >
+                    <el-button type="primary" text @click="handlerUpdateStatus(scope.row.id, 2)" v-if="scope.row.statusCode == '1'" v-permission="'PUT/employee-status'"
+                        >离职</el-button
+                    >
+                    <el-button type="danger" text @click="handleResetPassword(scope.row.id)" v-permission="'PUT/employee-password'">重置密码</el-button>
                 </template>
-                <template #columns>
-                    <el-table-column type="index" label="序号" width="80" />
-                    <el-table-column label="头像" width="80">
-                        <template #default="scope">
-                            <el-avatar :size="30" v-if="scope.row.avatarImgUrl" :src="scope.row.avatarImgUrl" />
-                            <el-avatar :size="30" v-else>{{ scope.row.name.substr(0, 1) }}</el-avatar>
-                        </template>
-                    </el-table-column>
-                    <el-table-column label="姓名" prop="name" width="100" />
-                    <el-table-column label="手机号码" prop="phoneNumber" width="120" />
-                    <el-table-column label="帐号状态" prop="accountStatus" width="100" />
-                    <el-table-column label="状态" prop="status" width="100" />
-                    <el-table-column label="部门" prop="departmentNames" width="100" />
-                    <el-table-column label="职位" prop="positionNames" width="100" />
-                    <el-table-column label="角色" prop="roleName" width="100" />
-                    <el-table-column label="性别" prop="gender" width="80" />
-                    <el-table-column label="工号" prop="jobNumber" width="100" />
-                    <el-table-column label="邮箱" prop="email" width="200" />
-                    <el-table-column label="创建时间" prop="createGmt" width="200" />
-                    <el-table-column label="修改时间" prop="modifiedGmt" width="200" />
-                    <el-table-column label="操作" fixed="right" width="220">
-                        <template #default="scope">
-                            <el-button type="primary" text @click="handleEdit(scope.row.id)" v-permission="'PUT/employee'">修改</el-button>
-                            <el-button
-                                type="primary"
-                                text
-                                @click="handlerUpdateAccountStatus(scope.row.id, 1)"
-                                v-if="scope.row.accountStatusCode == '2'"
-                                v-permission="'PUT/employee-account-status'"
-                                >启用</el-button
-                            >
-                            <el-button
-                                type="primary"
-                                text
-                                @click="handlerUpdateAccountStatus(scope.row.id, 2)"
-                                v-if="scope.row.accountStatusCode == '1'"
-                                v-permission="'PUT/employee-account-status'"
-                                >禁用</el-button
-                            >
-                            <el-button type="primary" text @click="handlerUpdateStatus(scope.row.id, 1)" v-if="scope.row.statusCode == '2'" v-permission="'PUT/employee-status'"
-                                >在职</el-button
-                            >
-                            <el-button type="primary" text @click="handlerUpdateStatus(scope.row.id, 2)" v-if="scope.row.statusCode == '1'" v-permission="'PUT/employee-status'"
-                                >离职</el-button
-                            >
-                            <el-button type="danger" text @click="handleResetPassword(scope.row.id)" v-permission="'PUT/employee-password'">重置密码</el-button>
-                        </template>
-                    </el-table-column>
-                </template>
-            </CSearchTable>
-        </el-col>
-    </el-row>
+            </el-table-column>
+        </template>
+    </CSearchTable>
     <EmployeeForm v-model:dialogVisible="dialogVisible" :dialogData="dialogData" @close="closeEmployeeForm" @confirm="confirmEmployeeForm" />
 </template>
 <script lang="ts" setup>
@@ -104,7 +93,6 @@ import { getPosition } from "@/service/system/position"
 import { getRole } from "@/service/system/role"
 import { ElMessage, ElMessageBox } from "element-plus"
 import EmployeeForm from "./components/EmployeeForm.vue"
-import CTree from "@/components/CTree/index.vue"
 
 // 部门树数据
 const treeData = ref<DepartmentTreeBO[]>([])
@@ -113,15 +101,13 @@ provide(
     "treeData",
     computed(() => treeData.value)
 )
-// 当前选中的节点
-const currentNodeKey = ref(0)
 const loading = ref(false)
 
 // 查询条件
 const initQueryModel = {
     pageNum: 1,
     pageSize: 10,
-    departmentId: 0,
+    departmentId: null,
     statusCode: null,
     name: null,
     phoneNumber: null
@@ -172,8 +158,7 @@ onMounted(() => {
 // 获取部门树
 const getDepartmentTree = () => {
     getDepartmentTreeData().then(async (response: Result<DepartmentTreeBO[]>) => {
-        const result = response
-        treeData.value = [{ id: 0, name: "部门树", children: [...result.data] }]
+        treeData.value = response.data
     })
 }
 // 获取职位列表
@@ -192,7 +177,6 @@ const listRole = () => {
 // 搜索
 const getTableData = () => {
     loading.value = true
-    queryModel.value.departmentId = currentNodeKey.value
     getEmployee(queryModel.value)
         .then((response: Result<EmployeeBO[]>) => {
             const result = response
@@ -207,11 +191,6 @@ const getTableData = () => {
 //  重置
 const reset = () => {
     queryModel.value = initQueryModel
-    getTableData()
-}
-// 树节点点击
-const handlerNodeClick = (key: any) => {
-    currentNodeKey.value = key
     getTableData()
 }
 // 新增
